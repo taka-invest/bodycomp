23f2b63c093e3a76a0954106607e533502c109c6
TAKA <naoki@maruyasusyounan.com>
2026-06-27 06:47:49 +0900

    Initial commit: BodyComp PWA

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'bodycomp-v1';
+const OFFLINE_ASSETS = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(OFFLINE_ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先 → PC更新がiPhoneにすぐ反映される
+// オフライン時はキャッシュにフォールバック
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      if (res && res.status === 200) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }).catch(() =>
+      caches.match(e.request).then(cached => cached || caches.match('/index.html'))
+    )
+  );
+});
